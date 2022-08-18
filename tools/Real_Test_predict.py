b8896aa221b371e2f2e810e43b0230f11b98799e
leon0719 <leon0970865551@gmail.com>
2022-08-18 16:32:42 +0800

    update

--- tools/Real_Test_predict.py
+++ tools/Real_Test_predict.py
@@ -1,15 +1,18 @@
 import os
 
 test_ch_img_path = '/workspace/test_data/ch/ch_img'
-
+test_en_img_path = '/workspace/test_data/en/en_img'
 os.chdir('/workspace/PaddleOCR')
-os.system(f'python3 tools/infer_rec.py -c configs/rec/PP-OCRv3/PP-OCRv3_rec.yml -o Global.pretrained_model=output/my_ocr_model/best_accuracy  Global.infer_img={test_ch_img_path}')
+os.system(f'python3 tools/infer_rec.py -c configs/rec/PP-OCRv3/PP-OCRv3_rec.yml -o Global.pretrained_model=output_v1/my_ocr_model/best_accuracy  Global.infer_img={test_ch_img_path}')
 path = '/workspace/PaddleOCR/output/rec/predicts_my_ocr_model.txt'
 #python3 tools/infer_rec.py -c configs/rec/PP-OCRv3/PP-OCRv3_rec.yml -o Global.pretrained_model=output/my_ocr_model/best_accuracy  Global.infer_img=/test_data/ch/crop/
 with open(path,'r',encoding='utf-8') as f :
     f = f.readlines()
     groud_truths = [line.strip().split('\t')[0].split('/')[-1].split('_')[:-1] for line in f]
     predicts = [line.strip().split('\t')[1] for line in f]
+
+if not os.path.exists('/workspace/tools/result'):
+    os.mkdir('/workspace/tools/result')
 
 os.chdir('/workspace/tools/result')
 with open('ground_truths.txt','w',encoding='utf-8') as f :
