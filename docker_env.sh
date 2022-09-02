caa18abeaff8d5b2f7aa3d4671002637109e8fe3
leon0719 <leon0970865551@gmail.com>
2022-09-02 14:47:08 +0800

    update

---
+++ docker_env.sh
@@ -0,0 +1,4 @@
+docker run --gpus all -it --name GPU3 -u $(id -u):$(id -g) \
+    -v /home/leon/taigiOCR:/workspace/ \
+    -v /nfs/TS-1635AX/WorkSpace/leon/GPU3:/train_data/ \
+    --shm-size=120g --ulimit memlock=-1 leonhilty/ocr_search:v1.0.6 /bin/bash
