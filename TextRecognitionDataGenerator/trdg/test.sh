a73c7643acb4452efedd7c822873ccad8b2aa40e
leon0719 <leon0970865551@gmail.com>
2022-09-07 21:36:32 +0800

    update

--- TextRecognitionDataGenerator/trdg/test.sh
+++ TextRecognitionDataGenerator/trdg/test.sh
@@ -1,4 +1,4 @@
-python run.py -c 200 -i dicts/ch/chinese_article.txt -t $(cat /proc/cpuinfo | grep "processor" | wc -l) -b 3 -fd fonts/train_font/ch/ --output_dir ../../test/test_img
+python run.py -c 200 -i dicts/ch/ch_article.txt -t $(cat /proc/cpuinfo | grep "processor" | wc -l) -b 3 -fd fonts/train_font/ch/ --output_dir ../../test/test_img
 python run.py -c 200 -i dicts/en/en_article.txt -t $(cat /proc/cpuinfo | grep "processor" | wc -l) -b 3 -fd fonts/train_font/en/ --output_dir ../../test/test_img
 python run.py -c 200 -i dicts/TAI_LO/TAI_LO_corpus.txt -t $(cat /proc/cpuinfo | grep "processor" | wc -l) -b 3 -fd fonts/train_font/TAI_LO/ --output_dir ../../test/test_img
 python run.py -c 200 -i dicts/POJ/POJ_corpus.txt -t $(cat /proc/cpuinfo | grep "processor" | wc -l) -b 3 -fd fonts/train_font/POJ/ --output_dir ../../test/test_img
