0106a417be77dbec3eacf7b3a5977f27128f609c
leon0719 <leon0970865551@gmail.com>
2022-09-10 12:21:44 +0800

    update

---
+++ PaddleOCR/download_pretrained-model.sh
@@ -0,0 +1,4 @@
+wget https://paddleocr.bj.bcebos.com/PP-OCRv3/chinese/ch_PP-OCRv3_rec_train.tar
+tar xf ch_PP-OCRv3_rec_train.tar
+rm -rf ch_PP-OCRv3_rec_train.tar
+mv ch_PP-OCRv3_rec_train Pretrained_model
