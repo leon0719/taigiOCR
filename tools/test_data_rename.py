f8f207dae2453621186bafdbb77dd364051b213a
leon0719 <leon0970865551@gmail.com>
2022-09-13 13:34:14 +0800

    update

--- tools/test_data_rename.py
+++ tools/test_data_rename.py
@@ -1,11 +1,12 @@
 import os
-with open('/home/leon/Paper_work/back_up_test_data/en/rec_gt.txt','r',encoding='utf8') as f:
+with open('/home/leon/taigiOCR/test_data/Hanji/rec_gt.txt', 'r', encoding='utf8') as f:
     f = f.readlines()
     line_list = [line.strip().split('\t') for line in f]
 
-img_path = '/home/leon/Paper_work/back_up_test_data/en/crop_img'
+img_path = '/home/leon/taigiOCR/test_data/Hanji'
 
 count = 1
 for line in line_list:
-    os.rename(os.path.join(img_path, line[0]),os.path.join(img_path, line[1].replace(' ','_')+f'_{count}'+'.jpg'))
+    os.rename(os.path.join(img_path, line[0]), os.path.join(
+        img_path, os.path.join('crop_img', line[1].replace(' ', '_')+f'_{count}'+'.jpg')))
     count += 1
