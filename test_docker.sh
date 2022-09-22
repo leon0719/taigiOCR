7a0c07c1480d497e1688b9d7cb18344672e61e66
leon0719 <leon0970865551@gmail.com>
2022-09-22 14:26:42 +0800

    update

--- test_docker.sh
+++ test_docker.sh
@@ -1,6 +1,6 @@
-docker run --gpus all -it --name test_GPU3 \
-    -v /nfs/GPU3/home/leon/taigiOCR/:/workspace/ \
-    -v /nfs/TS-1635AX/WorkSpace/leon/GPU3:/train_data/ \
+docker run --gpus all -it --name test_GPU2 -u $(id -u):$(id -g)\
+    -v /nfs/GPU2/home/leon/taigiOCR/:/workspace/ \
+    -v /nfs/TS-1635AX/WorkSpace/leon/GPU2:/train_data/ \
     --shm-size=120g --ulimit memlock=-1 leonhilty/ocr_search:v1.0.7 /bin/bash
 
 # -u $(id -u):$(id -g)
