3aa3f7b24b2cf740f9402b3b130e9e0dfe0d2702
leon0719 <leon0970865551@gmail.com>
2022-09-26 17:24:26 +0800

    update

--- PaddleOCR/export_model.sh
+++ PaddleOCR/export_model.sh
@@ -1,1 +1,1 @@
-python3 tools/export_model.py -c configs/rec/PP-OCRv3/PP-OCRv3_rec.yml -o Global.pretrained_model=output/my_ocr_model/best_accuracy  Global.save_inference_dir=./rec_inference/
+python3 tools/export_model.py -c configs/rec/PP-OCRv3/PP-OCRv3_rec.yml -o Global.pretrained_model=output/my_ocr_model/best_accuracy  Global.save_inference_dir=./rec_inference3/
