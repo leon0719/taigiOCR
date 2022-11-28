dc29e37cebe3bc24c4a64ea8ef7cca69a1f5806b
leon0719 <leon0970865551@gmail.com>
2022-11-28 14:51:01 +0800

    update

--- tools/test_data_rename.py
+++ tools/test_data_rename.py
@@ -1,12 +1,13 @@
 import os
-with open('/home/leon/taigiOCR/Hanji/rec_gt.txt', 'r', encoding='utf8') as f:
+with open('/home/leon/taigiOCR/test_data_expand/Hanji/rec_gt.txt', 'r', encoding='utf8') as f:
     f = f.readlines()
     line_list = [line.strip().split('\t') for line in f]
 
-img_path = '/home/leon/taigiOCR/Hanji'
+img_path = '/home/leon/taigiOCR/test_data_expand/Hanji'
 
 count = 1
 for line in line_list:
     os.rename(os.path.join(img_path, line[0]), os.path.join(
         img_path, os.path.join('crop_img', line[1].replace('/', ' ').replace(' ', '_')+f'_{count}'+'.jpg')))
     count += 1
+
