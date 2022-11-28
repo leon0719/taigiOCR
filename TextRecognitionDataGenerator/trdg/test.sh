ef0f536302783014d94a7226819710fe716d2aa4
leon0719 <leon0970865551@gmail.com>
2022-11-28 15:32:49 +0800

    update

--- TextRecognitionDataGenerator/trdg/test.sh
+++ TextRecognitionDataGenerator/trdg/test.sh
@@ -1,6 +1,6 @@
-python run.py -c 200 -f 48 -i dicts/ch/ch_article2.txt -t $(cat /proc/cpuinfo | grep "processor" | wc -l) -id images/Hanji -fd fonts/ch/
-python run.py -c 200 -f 48 -i dicts/en/en_article2.txt -t $(cat /proc/cpuinfo | grep "processor" | wc -l) -id images/en -fd fonts/en/
-python run.py -c 200 -f 48 -i dicts/TAI_LO/TAI_LO_corpus2.txt -t $(cat /proc/cpuinfo | grep "processor" | wc -l) -id images/POJ -fd fonts/TAI_LO/
-python run.py -c 200 -f 48 -i dicts/POJ/POJ_corpus2.txt -t $(cat /proc/cpuinfo | grep "processor" | wc -l) -id images/POJ -fd fonts/POJ/
-python run.py -c 200 -f 48 -i dicts/HAN_LO/HAN_LO_corpus2.txt -t $(cat /proc/cpuinfo | grep "processor" | wc -l) -id images/HAN_LO -fd fonts/HAN_LO/
-python run.py -c 200 -f 48 -i dicts/jp/jp_corpus2.txt -t $(cat /proc/cpuinfo | grep "processor" | wc -l) -id images/Hanji/ -fd fonts/jp/
+python run.py -c 200 -f 48 -i dicts/ch/test.txt -t $(cat /proc/cpuinfo | grep "processor" | wc -l) -id images/Hanji -fd fonts/ch/
+python run.py -c 200 -f 48 -i dicts/en/test.txt -t $(cat /proc/cpuinfo | grep "processor" | wc -l) -id images/en -fd fonts/en/
+python run.py -c 200 -f 48 -i dicts/TAI_LO/test.txt -t $(cat /proc/cpuinfo | grep "processor" | wc -l) -id images/POJ -fd fonts/TAI_LO/
+python run.py -c 200 -f 48 -i dicts/POJ/test.txt -t $(cat /proc/cpuinfo | grep "processor" | wc -l) -id images/POJ -fd fonts/POJ/
+python run.py -c 200 -f 48 -i dicts/HAN_LO/test.txt -t $(cat /proc/cpuinfo | grep "processor" | wc -l) -id images/HAN_LO -fd fonts/HAN_LO/
+python run.py -c 200 -f 48 -i dicts/jp/test.txt -t $(cat /proc/cpuinfo | grep "processor" | wc -l) -id images/Hanji/ -fd fonts/jp/
