ecb363407e4e614c39486324a1a4e759fb579335
leon0719 <leon0970865551@gmail.com>
2022-12-20 20:43:59 +0800

    update

--- docker_env.sh
+++ docker_env.sh
@@ -1,6 +1,6 @@
 docker run --gpus all -it --name GPU5 -u $(id -u):$(id -g) \
     -v /home/leon/taigiOCR:/workspace/ \
-    -v /nfs/TS-1635AX/WorkSpace/leon/GPU5/:/train_data/ \
+    -v /nfs/TS-1635AX/WorkSpace/leon/gpu5/:/train_data/ \
     --shm-size=140g --ulimit memlock=-1 leonhilty/ocr_search:v1.0.8 /bin/bash
 
 # -u $(id -u):$(id -g)
